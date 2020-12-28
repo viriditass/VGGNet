a88c3441f72778a2d56039d992df8c0cc37dc037
Jipoleon <67452897+viriditass@users.noreply.github.com>
2020-12-28 19:06:55 +0900

    add test mode

--- train.py
+++ train.py
@@ -18,14 +18,23 @@ print(classes)
 data_dir = './data'
 batch_size = 64
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+mode = 'train'
 
 ## Train
-dataset_train = Dataset(data_dir=os.path.join(data_dir, 'train'))
-loader_train = DataLoader(dataset_train, batch_size=batch_size, shuffle=True, num_workers=0)
+if mode == 'train':
+    dataset_train = Dataset(data_dir=os.path.join(data_dir, 'train'))
+    loader_train = DataLoader(dataset_train, batch_size=batch_size, shuffle=True, num_workers=0)
 
-num_data_train = len(dataset_train)
+    num_data_train = len(dataset_train)
 
-num_batch_train = np.ceil(num_data_train / batch_size)
+    num_batch_train = np.ceil(num_data_train / batch_size)
+else:
+    dataset_test = Dataset(data_dir=os.path.join(data_dir, 'test'))
+    loader_test = DataLoader(dataset_test, batch_size=batch_size, shuffle=False, num_workers=0)
+
+    num_data_test = len(dataset_test)
+
+    num_batch_test = np.ceil(num_data_test / batch_size)
 
 # network load
 VGG_types = {
@@ -52,35 +61,61 @@ optim = torch.optim.Adam(net.parameters(),lr=0.00001)
 st_epoch = 0
 num_epoch = 50
 ckpt_dir = './checkpoint'
+train_continue = 'on'
 
-for epoch in range(st_epoch + 1, num_epoch + 1):
-    net.train()
+# TRAIN MODE
+if mode == 'train':
+    if train_continue == 'on':
+        net, optim, st_epoch = load(ckpt_dir=ckpt_dir, net=net, optim=optim)
+        
+    for epoch in range(st_epoch + 1, num_epoch + 1):
+        net.train()
+        loss_arr = []
+
+        for batch, data in enumerate(loader_train, 1):
+            # forward pass
+            label = data['label'].to(device)
+            input = data['input'].to(device)
+
+            label = torch.squeeze(label)
+            input = torch.squeeze(input)
+
+            output = net(input)
+
+            # backward pass
+            optim.zero_grad()
+
+            loss = fn_loss(output, label)
+            loss.backward()
+
+            optim.step()
+
+            # loss function
+            loss_arr += [loss.item()]
+
+            print("TRAIN: EPOCH %04d / %04d | BATCH %04d / %04d | LOSS %.4f" %
+                      (epoch, num_epoch, batch, num_batch_train, np.mean(loss_arr)))
+
+
+            if epoch % 10 == 0:
+                save(ckpt_dir=ckpt_dir, net=net, optim=optim, epoch=epoch)
+
+# TEST MODE
+else:
+    net, optim, st_epoch = load(ckpt_dir=ckpt_dir, net=net, optim=optim)
+
     loss_arr = []
 
-    for i, data in enumerate(loader_train, 1):
-        # forward pass
+    for batch, data in enumerate(loader_test, 1):
         label = data['label'].to(device)
         input = data['input'].to(device)
 
-        label = torch.squeeze(label)
-        input = torch.squeeze(input)
-        
         output = net(input)
 
-        # backward pass
-        optim.zero_grad()
-
         loss = fn_loss(output, label)
-        loss.backward()
-
-        optim.step()
-
-        # loss function
-        loss_arr += [loss.item()]
 
         print("TRAIN: EPOCH %04d / %04d | BATCH %04d / %04d | LOSS %.4f" %
-                  (epoch, num_epoch, i, num_batch_train, np.mean(loss_arr)))
+                    (epoch, num_epoch, batch, num_batch_train, np.mean(loss_arr)))
 
-
-        if epoch % 10 == 0:
-            save(ckpt_dir=ckpt_dir, net=net, optim=optim, epoch=epoch)
+    print("AVERAGE TEST: BATCH %04d / %04d | LOSS %.4f" %
+                        (batch, num_batch_test, np.mean(loss_arr)))
